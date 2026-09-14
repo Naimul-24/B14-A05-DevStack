464667b21e7d3e5fd4966dd2f2ba36aa5b987943
Md. Naimul Hossain <154729788+Naimul-24@users.noreply.github.com>
2026-09-14 14:59:45 +0600

    feat: add React entry point

---
+++ src/main.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import App from './App';
+import './styles.css';
+
+createRoot(document.getElementById('root')).render(<React.StrictMode><App /><ToastContainer position="top-right" autoClose={2200} /></React.StrictMode>);
